52ce12b9ab3c9ccc3790fbdd8b6ada4abc1227fd
alejandrodob <alejandrodob@gmail.com>
2015-10-31 16:58:22 +0100

    print for function type

--- my_python/printer.py
+++ my_python/printer.py
@@ -1,4 +1,4 @@
-from _types import List, Vector, Hash
+from _types import List, Vector, Hash, Function
 
 def pr_str(data_structure):
     if type(data_structure) == List:
@@ -10,6 +10,8 @@ def pr_str(data_structure):
     elif type(data_structure) == Hash:
         result = List([pr_str(elem) for elem in data_structure])
         result = '{' + " ".join(result) + '}'
+    elif type(data_structure) == Function:
+        return '#<function>'
     else:
         result = str(data_structure)
     return result
