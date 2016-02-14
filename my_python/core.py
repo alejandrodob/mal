09cd7aab0889fd0e9b7263a6557476dd8832ec66
alejandrodob <alejandrodob@gmail.com>
2016-02-14 20:00:54 +0100

    Add core functions

--- my_python/core.py
+++ my_python/core.py
@@ -1,6 +1,26 @@
+from _types import List
+
+def _list(*args):
+    return List(args)
+
+def _count(expr):
+    if expr == None:
+        return 0
+    return len(expr)
+
 ns = {
     '+': lambda a,b: a+b,
     '-': lambda a,b: a-b,
     '*': lambda a,b: a*b,
     '/': lambda a,b: int(a/b),
+    '<': lambda a,b: a<b,
+    '<=': lambda a,b: a<=b,
+    '>': lambda a,b: a>b,
+    '>=': lambda a,b: a>=b,
+    '=': lambda a,b: a==b,
+
+    'list': _list,
+    'list?': lambda a: type(a) == List,
+    'empty?': lambda a: len(a) == 0,
+    'count': _count,
 }
