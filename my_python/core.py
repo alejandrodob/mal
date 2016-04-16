37a9be73b050a0055452d6703c234eba4f74d641
alejandrodob <alejandrodob@gmail.com>
2016-04-16 10:48:01 +0200

    Step4 completed

--- my_python/core.py
+++ my_python/core.py
@@ -1,4 +1,5 @@
-from _types import List
+from _types import List, String
+from printer import pr_str
 
 def _list(*args):
     return List(args)
@@ -7,6 +8,20 @@ def _count(expr):
     if expr == None:
         return 0
     return len(expr)
+
+def _pr_str(*args):
+    return String(" ".join(map(lambda exp: pr_str(exp, True), args)))
+
+def _str(*args):
+    return String("".join(map(lambda exp: pr_str(exp, False), args)))
+
+def _prn(*args):
+    print String(" ".join(map(lambda exp: pr_str(exp, True), args)))
+    return None
+
+def _println(*args):
+    print String(" ".join(map(lambda exp: pr_str(exp, False), args)))
+    return None
 
 ns = {
     '+': lambda a,b: a+b,
@@ -23,4 +38,9 @@ ns = {
     'list?': lambda a: type(a) == List,
     'empty?': lambda a: len(a) == 0,
     'count': _count,
+
+    'pr-str': _pr_str,
+    'str': _str,
+    'prn': _prn,
+    'println': _println,
 }
--- my_python/printer.py
+++ my_python/printer.py
@@ -1,19 +1,21 @@
 from _types import List, Vector, Hash, Function, String, Keyword
 
-def pr_str(data_structure):
+def pr_str(data_structure, print_readably=True):
     if type(data_structure) == List:
-        result = List([pr_str(elem) for elem in data_structure])
+        result = List([pr_str(elem, print_readably) for elem in data_structure])
         result = '(' + " ".join(result) + ')'
     elif type(data_structure) == Vector:
-        result = List([pr_str(elem) for elem in data_structure])
+        result = List([pr_str(elem, print_readably) for elem in data_structure])
         result = '[' + " ".join(result) + ']'
     elif type(data_structure) == Hash:
-        result = List([pr_str(elem) for elem in data_structure])
+        result = List([pr_str(elem, print_readably) for elem in data_structure])
         result = '{' + " ".join(result) + '}'
     elif type(data_structure) == Function:
         return '#<function>'
     elif type(data_structure) == String:
-        return '"' + data_structure.replace('"', '\\"') + '"'
+        if print_readably:
+            return '"' + data_structure.encode('unicode_escape').decode('latin1').replace('"', '\\"') + '"'
+        return data_structure
     elif type(data_structure) == Keyword:
         return ':' + data_structure
     elif data_structure is True:
